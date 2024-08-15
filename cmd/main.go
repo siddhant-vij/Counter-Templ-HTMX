012ac2d2da63fba7daeee9f7c76920cf4731f35a
Siddhant Vij <siddhantvij201@gmail.com>
2024-08-16 02:15:57 +0530

    feat: session counters logic

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
 
 	"github.com/siddhant-vij/Counter-Templ-HTMX/templates"
 )
@@ -12,9 +15,11 @@ type GlobalState struct {
 }
 
 var global GlobalState
+var sessionManager *scs.SessionManager
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
-	pageComponent := templates.Page(global.Count, 0)
+	sessionCount := sessionManager.GetInt(r.Context(), "sessionCount")
+	pageComponent := templates.Page(global.Count, sessionCount)
 	pageComponent.Render(r.Context(), w)
 }
 
@@ -25,14 +30,25 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		global.Count++
 	}
 
+	if r.Form.Has("session") {
+		curCount := sessionManager.GetInt(r.Context(), "sessionCount")
+		curCount++
+		sessionManager.Put(r.Context(), "sessionCount", curCount)
+	}
+
 	getHandler(w, r)
 }
 
 func main() {
-	fileServer := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	sessionManager = scs.New()
+	sessionManager.Lifetime = 24 * time.Hour
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
 			postHandler(w, r)
 			return
@@ -40,6 +56,8 @@ func main() {
 		getHandler(w, r)
 	})
 
+	muxWithSessionMiddleware := sessionManager.LoadAndSave(mux)
+
 	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", muxWithSessionMiddleware))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/siddhant-vij/Counter-Templ-HTMX
 
 go 1.23.0
 
-require github.com/a-h/templ v0.2.747
+require (
+	github.com/a-h/templ v0.2.747
+	github.com/alexedwards/scs/v2 v2.8.0
+)
